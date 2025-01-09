522097a046da672a6cadc3c98f074b93a72bddc1
chenliangwei <chan.we@outlook.com>
2025-01-09 15:03:26 +0800

    feat: types

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
-      entry:  path.resolve(__dirname, 'lib/index.tsx'),
+      entry: path.resolve(__dirname, 'lib/index.tsx'),
       name: 'NiceModalVue',
       fileName: 'index',
     },
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), dts()],
 })
